41e5aa5ed8553f8f7b2c91ce1ecb0172ed187cd9
merttro <mertalikorkmaz023@gmail.com>
2026-06-01 19:39:04 +0300

    ADD: PrimeCheck, PrimeCheckTest and UnitTest Workflow

---
+++ PrimeCheckTest.java
@@ -0,0 +1,39 @@
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PrimeCheckTest {
+    // ---------------------------------------------------------- //
+    @Test
+    public void TestNegativeNumbers() {
+        for (int i = -10; i < 0; ++i) {
+            assertEquals("Test negative number " + i, false, PrimeCheck.isPrime(i));
+        }
+    }
+
+    // ---------------------------------------------------------- //
+    @Test
+    public void TestEdgeCases() {
+        assertEquals("Test 0", false, PrimeCheck.isPrime(0));
+        assertEquals("Test 1", false, PrimeCheck.isPrime(1));
+        assertEquals("Test 2", true,  PrimeCheck.isPrime(2));
+    }
+
+    // ---------------------------------------------------------- //
+    @Test
+    public void testPrimes() {
+        int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+        for (int p : primes) {
+            assertEquals("Test prime " + p, true, PrimeCheck.isPrime(p));
+        }
+    }
+
+    // ---------------------------------------------------------- //
+    @Test
+    public void testNonPrimes() {
+        int[] nonPrimes = {4, 6, 8, 9, 10, 12, 15, 25, 49};
+        for (int n : nonPrimes) {
+            assertEquals("Test non-prime " + n, false, PrimeCheck.isPrime(n));
+        }
+    }
+}
